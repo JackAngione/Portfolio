4e0a8e7065e53a9470e8dfd9c64b3385be71a1a2
JackAngione <jangio1@students.towson.edu>
2023-05-17 22:18:40 -0400

    Initialization
    initialized Vite webapp and the backend

---
+++ web_app/src/homepage.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './homepage.css'
+
+function Homepage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default Homepage
