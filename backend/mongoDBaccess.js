1fcb511cfb3b2b2f2ca1e45186584afbdd05479d
JackAngione <jangio1@students.towson.edu>
2023-05-20 22:57:53 -0400

    searches every field in database
    now searches across title, description, source, and keywords to find matches

--- backend/mongoDBaccess.js
+++ backend/mongoDBaccess.js
@@ -8,8 +8,8 @@ const client = new MongoClient(connectionString)
 
 async function searchTutorials(searchQuery) {
     const collection = client.db("KNOWLEDGE").collection("tutorials")
-    const regex = new RegExp(searchQuery, 'i')
-    const findString = { $or: [{title: regex}, {description: regex}]}
+    const regex = new RegExp(decodeURIComponent(searchQuery), 'i')
+    const findString = { $or: [{title: regex}, {description: regex}, {keywords: regex}, {source: regex}]}
     const documents = await collection.find(findString).toArray()
     console.log(JSON.stringify(documents))
     return JSON.stringify(documents)
